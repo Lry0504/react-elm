1854d8cef93d99565def8de60979a4cc3b2fc74b
lry <lry885175073@163.com>
2018-12-13 21:14:01 +0800

    解决异步加载报错问题

--- src/utils/asyncComponent.jsx
+++ src/utils/asyncComponent.jsx
@@ -4,27 +4,25 @@ import React, { Component } from 'react';
  *  @param {*}  importComponent
  */
 export default function asyncComponent(importComponent) {
-    class AsyncComponent extends Component {
-        constructor(props) {
-            super(props);
+  class AsyncComponent extends Component {
+    constructor(props) {
+        super(props);
 
-            this.state = {
-                component: null
-            };
-        }
-        async componentDidMount() {
-            const { default: component } = await importComponent();
-            this.setState({
-                component
-            });
-        }
-        render() {
-            const C = this.state.component;
-
-            return (
-                C ? <C {...this.props} /> : null
-            )
-        }
+        this.state = {
+            component: null
+        };
     }
-    return AsyncComponent;
+    async componentDidMount() {
+        const { default: component } = await importComponent();
+        this.setState({
+            component: component
+        });
+    }
+    render() {
+        const C = this.state.component;
+        return C ? <C {...this.props} /> : null;
+    }
+  }
+
+  return AsyncComponent
 }
