e18fd798087e55f07c05b99357b334ff5a038915
lry <lry885175073@163.com>
2018-12-14 16:21:05 +0800

    全局配置未生效，api直接补全，暂不考虑该问题

--- src/utils/config/envconfig.js
+++ src/utils/config/envconfig.js
@@ -3,9 +3,10 @@
 //路径配置
 let baseUrl = '';
 let imgUrl;
+//开发环境
 if (process.env.NODE_ENV === 'development') {
     imgUrl = '//elm.cangdu.org/img/';
-} else if (process.env.NODE_ENV === 'production') {
+} else if (process.env.NODE_ENV === 'production') { //生产环境
     baseUrl = '//elm.cangdu.org';
     imgUrl = '//elm.cangdu.org/img/';
 }
