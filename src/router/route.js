cd8378f83fbe15b118858cd251ad5d3ac8067d79
lry <lry885175073@163.com>
2018-12-14 16:21:27 +0800

    添加首页路由

--- src/router/route.js
+++ src/router/route.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import asyncComponent from '../utils/asyncComponent';
 
-//const Footer = asyncComponent( () => import('../components/Footer/Footer') )
+const Home = asyncComponent( () => import('../pages/Home/Home') )
 //const Header = asyncComponent( () => import('../components/Header/Header') )
 
 export default class RouteConfig extends Component {
@@ -11,12 +11,7 @@ export default class RouteConfig extends Component {
             <Router>
                 <Switch>
                     <Route path="/home" exact component={Home} /> {/*首页路由*/}
-                    <Route path="/login" component={Login} /> {/*登录路由*/}
-                    <Route path="/userinfo" component={UserInfo} /> {/*个人信息路由*/}
-                    <Route path="/user" component={User} /> {/*个人中心路由*/}
-                    <Route path="/setuserinfo" component={SetUserInfo} /> {/*设置用户信息路由*/}
-                    <Route path="/shopdetails/:id" component={ShopDetails} /> {/*商店详情路由*/}
-                    <Route path="/food/:geohash/:id/:title" component={Food} /> {/*食物页面路由*/}
+                   
                     <Redirect exact from="/" to="/home" />  {/*重定向到 -->> 首页路由*/}
                 </Switch>
             </Router>
