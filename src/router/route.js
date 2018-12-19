b8f30fa3cbf13f2c41732d717a01156a1be23098
lry <lry885175073@163.com>
2018-12-19 21:47:58 +0800

    新增ShopDetails店铺详情路由

--- src/router/route.js
+++ src/router/route.js
@@ -4,6 +4,7 @@ import asyncComponent from '../utils/asyncComponent';
 
 const Home = asyncComponent( () => import('../pages/Home/Home') )
 const Food = asyncComponent( () => import('../pages/Food/Food') )
+const ShopDetails = asyncComponent( () => import('../pages/ShopDetails/ShopDetails') )
 
 export default class RouteConfig extends Component {
     render () {
@@ -11,7 +12,8 @@ export default class RouteConfig extends Component {
             <Router>
                 <Switch>
                     <Route path="/home" exact component={Home} /> {/*首页路由*/}
-        <Route path="/food/:geohash/:id/:title" component={Food} /> {/*首页nav导航食物页面*/}
+                    <Route path="/food/:geohash/:id/:title" component={Food} /> {/*首页nav导航食物页面*/}
+                    <Route path="/shopDetails/:id" component={ShopDetails} />
                     <Redirect exact from="/" to="/home" />  {/*重定向到 -->> 首页路由*/}
                 </Switch>
             </Router>
