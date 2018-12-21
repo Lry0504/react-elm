b7188fb7f7d09d2029e2f6e1d9224cddc7c15e7d
lry <lry885175073@163.com>
2018-12-21 14:27:33 +0800

    添加店铺详情路由与个人中心路由

--- src/router/route.js
+++ src/router/route.js
@@ -5,6 +5,7 @@ import asyncComponent from '../utils/asyncComponent';
 const Home = asyncComponent( () => import('../pages/Home/Home') )
 const Food = asyncComponent( () => import('../pages/Food/Food') )
 const ShopDetails = asyncComponent( () => import('../pages/ShopDetails/ShopDetails') )
+const User = asyncComponent( () => import('../pages/User/User') )
 
 export default class RouteConfig extends Component {
     render () {
@@ -14,6 +15,7 @@ export default class RouteConfig extends Component {
                     <Route path="/home" exact component={Home} /> {/*首页路由*/}
                     <Route path="/food/:geohash/:id/:title" component={Food} /> {/*首页nav导航食物页面*/}
                     <Route path="/shop/:id" component={ShopDetails} />
+                    <Route path="/user" component={User} />  {/*个人中心路由*/}
                     <Redirect exact from="/" to="/home" />  {/*重定向到 -->> 首页路由*/}
                 </Switch>
             </Router>
