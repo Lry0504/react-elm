e189b652b247e9d4d9c7029adc88b5719d573b9b
lry <lry885175073@163.com>
2018-12-24 09:04:20 +0800

    添加登录路由

--- src/router/route.js
+++ src/router/route.js
@@ -6,6 +6,7 @@ const Home = asyncComponent( () => import('../pages/Home/Home') )
 const Food = asyncComponent( () => import('../pages/Food/Food') )
 const ShopDetails = asyncComponent( () => import('../pages/ShopDetails/ShopDetails') )
 const User = asyncComponent( () => import('../pages/User/User') )
+const Login = asyncComponent( () => import('../pages/Login/Login') )
 
 export default class RouteConfig extends Component {
     render () {
@@ -16,6 +17,7 @@ export default class RouteConfig extends Component {
                     <Route path="/food/:geohash/:id/:title" component={Food} /> {/*首页nav导航食物页面*/}
                     <Route path="/shop/:id" component={ShopDetails} />
                     <Route path="/user" component={User} />  {/*个人中心路由*/}
+                    <Route path="/login" component={Login} /> {/*登录路由*/}
                     <Redirect exact from="/" to="/home" />  {/*重定向到 -->> 首页路由*/}
                 </Switch>
             </Router>
